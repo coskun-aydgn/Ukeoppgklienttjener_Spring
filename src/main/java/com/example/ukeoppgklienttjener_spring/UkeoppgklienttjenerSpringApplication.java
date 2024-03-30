2e25932d6d4e765a79a915442167f95cfa0e89b2
47462 <coskun.aydn@gmail.com>
2024-03-30 13:58:51 +0100

    Initial commit

---
+++ src/main/java/com/example/ukeoppgklienttjener_spring/UkeoppgklienttjenerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgklienttjener_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkeoppgklienttjenerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkeoppgklienttjenerSpringApplication.class, args);
+    }
+
+}
